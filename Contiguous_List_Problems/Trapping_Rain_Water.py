b2e609acaacdb9414cab315d75fbdb75951ca5cd
SDWoo <damjang67@naver.com>
2022-01-06 18:19:54 +0900

    1/6 study - 2

--- Contiguous_List_Problems/Trapping_Rain_Water.py
+++ Contiguous_List_Problems/Trapping_Rain_Water.py
@@ -2,7 +2,7 @@
 # compute how much water it can trap after raining.
 # Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 # Output: 6
-# Solution
+# Solution 1
 class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
@@ -22,4 +22,25 @@ class Solution:
                 volume += right_max - height[right]
                 right -= 1
 
+        return volume
+
+# Solution 2
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
         return volume
