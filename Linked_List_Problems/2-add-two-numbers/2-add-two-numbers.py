0becbb20b9a8df816255176fe27b3f8f146f434c
SDWoo <damjang67@naver.com>
2022-01-07 16:53:15 +0900

    1/7 study

--- Linked_List_Problems/2-add-two-numbers/2-add-two-numbers.py
+++ Linked_List_Problems/2-add-two-numbers/2-add-two-numbers.py
@@ -40,3 +40,23 @@ class Solution:
         
         result = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
         return self.toReversedLinkedList(str(result))
+
+# Solution2
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> Listnode:
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                sum+= l1.val
+                l1 = l1.next
+            if l2:
+                sum+= l2.val
+                l2 = l2.next
+
+        carry.val = divmod(sum + carry, 10)
+        head.next = ListNode(val)
+        head = head.next
+
+        return root.next
