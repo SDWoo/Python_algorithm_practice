7c9c70b1da4f153b311dbb03585fc71f54a86374
SDWoo <damjang67@naver.com>
2022-01-15 01:04:07 +0900

    1/15 study

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,3 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         return list(itertools.combinations(range(1,n+1), k))
+
+
+class Solution2:
+    def combine2(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        def dfs(elements, start: int, k: int):
+            if k == 0:
+                result.append(elements[:])
+                return
+
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k - 1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return result
