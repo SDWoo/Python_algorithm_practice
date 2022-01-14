c65cbaa60cdf243d7fac6c799fc8d4fb880e2b2c
SDWoo <damjang67@naver.com>
2022-01-14 22:17:41 +0900

    Time: 314 ms (70.98%), Space: 16.6 MB (93.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,25 @@
 class Solution:
-    grid: List[List[str]]  # class member 변수
-    n_row: int
-    n_col: int
-
-    def dfs(self, i, j):
-        if i < 0 or i >= self.n_row or j < 0 or j >= self.n_col or self.grid[i][j] == '0':
-            return
-
-        self.grid[i][j] = '0'
-        self.dfs(i-1, j)  # down
-        self.dfs(i+1, j)  # up
-        self.dfs(i, j-1)  # left
-        self.dfs(i, j+1)  # right
-
+    
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.grid = grid
-        self.n_row, self.n_col = len(grid), len(grid[0])
-        n_island = 0
-        for i in range(self.n_row):
-            for j in range(self.n_col):
+        def dfs(i:int, j:int):
+            if i < 0  or i >= len(grid) or \
+                j < 0 or j >= len(grid[0]) or \
+                grid[i][j] != '1':
+                    return
+            
+            grid[i][j] = '0'
+        
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    self.dfs(i, j)
-                    n_island += 1
-        return n_island
+                    dfs(i, j)
+                    count += 1
+                    
+        return count
+    
