eb81bc351b4cc52c10e263d8291aabbaa5d86682
SDWoo <damjang67@naver.com>
2022-01-21 13:48:10 +0900

    1/21 study

--- Stack,Queue_Problems/316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ Stack,Queue_Problems/316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -7,10 +7,24 @@ class Solution:
             if char in seen:
                 continue
             while stack and char < stack[-1] and counter[stack[-1]] > 0:
-                print(stack[-1])
                 seen.remove(stack.pop())
                 
             stack.append(char)
             seen.add(char)
             
         return ''.join(stack)
+
+# Solution2
+class Solution2:
+    def removeDuplicateLetters2(self, s: str) -> str:
+        print(s)
+        print(set(s))
+        print(sorted(set(s)))
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters2(suffix.replace(char, ''))
+        return ''
+
+
+
