41bc8328125d223ffcd959bd712635f3557d93ce
SDWoo <damjang67@naver.com>
2022-01-22 16:15:54 +0900

    1/22 study

--- HashTable/347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ HashTable/347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -11,3 +11,8 @@ class Solution:
             topk.append(heapq.heappop(freqs_heap)[1])
             
         return topk
+
+# Solution2:
+clas Solution2:
+    def topk(self,nums: List[int], k: int) -> List[int]:
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
