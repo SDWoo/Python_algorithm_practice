aacd36c15b188a077b5a7723a46f69888ca308b2
SDWoo <damjang67@naver.com>
2022-01-23 15:39:50 +0900

    1/23 study

--- Graph/207-course-schedule/207-course-schedule.py
+++ Graph/207-course-schedule/207-course-schedule.py
@@ -26,3 +26,32 @@ class Solution:
                 return False
             
         return True
+
+# Solution2
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+            print(graph)
+        traced = set()
+
+        def dfs(i):
+            if i in traced:
+                return False
+
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+            traced.remove(i)
+
+            return True
+
+        for x in list(graph):
+            if not dfs(x):
+                return False
+
+        return True
